810be222f68fb685294994a0f1cf18d44941661b
melissikiss <melissikiss@gmail.com>
2021-02-18 12:28:57 -0800

    added validation for name, email, and message

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,40 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 
 function Contact() {
-    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
-    
+
+    const [errorMessage, setErrorMessage] = useState('');
+
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value}) // targets name attribute
+        if (e.target.name === 'email') {
+            const validEmail = validateEmail(e.target.value);
+            console.log(validEmail)
+            if (!validEmail) {
+                setErrorMessage("Sorry, your email is invalid!");
+            }
+            else {
+                setErrorMessage("");
+            }
+        }
+        else {
+            if (!e.target.value.length) {
+                setErrorMessage(`Sorry, a ${e.target.name} is required!`);
+            }
+            else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value }); // targets name attribute
+        }
     }
 
     function handleSubmit(e) {
-        e.preventDefault(); 
+        e.preventDefault();
         console.log(formState);
     }
 
@@ -18,20 +42,25 @@ function Contact() {
         <section className="container mt-5">
             <div className="row justify-content-center">
                 <div className="col-8 align-self-center">
-                    <form id ="contact" onSubmit={handleSubmit}>
+                    <form id="contact" onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label className="form-label form-text">name</label>
-                            <input onChange={handleChange} type="text" name="name" className="form-control" id="name" aria-describedby="emailHelp" defaultValue={name}/>
+                            <input onBlur={handleChange} type="text" name="name" className="form-control" id="name" aria-describedby="emailHelp" defaultValue={name} />
                         </div>
                         <div className="mb-3">
                             <label className="form-label form-text">email</label>
-                            <input onChange={handleChange} type="email" name="email" className="form-control" id="email" defaultValue={email}/>
+                            <input onBlur={handleChange} type="email" name="email" className="form-control" id="email" defaultValue={email} />
                         </div>
                         <div className="form-floating">
                             <label className="form-text">message</label>
-                            <textarea onChange={handleChange} name="message" className="form-control" placeholder="leave a message and I'll get back to you soon! :D" defaultValue={message} id="message" style={{ height: "100px" }}></textarea>
+                            <textarea onBlur={handleChange} name="message" className="form-control" placeholder="leave a message and I'll get back to you soon! :D" defaultValue={message} id="message" style={{ height: "100px" }}></textarea>
                         </div>
-                        <button type="submit" className="btn contact-btn mt-4">enter</button>
+                        {errorMessage && (
+                            <div>
+                                <p className="error-msg mt-2">{errorMessage}</p>
+                            </div>
+                        )}
+                        <button type="submit" className="btn contact-btn mt-3">enter</button>
                     </form>
                 </div>
             </div>
