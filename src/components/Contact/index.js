5c0a9ef19dec390ec002e319555d4bbe41b9962e
melissikiss <melissikiss@gmail.com>
2021-02-18 12:06:18 -0800

    contact form validation basic structure created

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const { name, email, message } = formState;
+    
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value}) // targets name attribute
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault(); 
+        console.log(formState);
+    }
+
     return (
         <section className="container mt-5">
             <div className="row justify-content-center">
                 <div className="col-8 align-self-center">
-                    <form>
+                    <form id ="contact" onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label className="form-label form-text">name</label>
-                            <input type="name" className="form-control" id="name" aria-describedby="emailHelp" />
+                            <input onChange={handleChange} type="text" name="name" className="form-control" id="name" aria-describedby="emailHelp" defaultValue={name}/>
                         </div>
                         <div className="mb-3">
                             <label className="form-label form-text">email</label>
-                            <input type="email" className="form-control" id="email" />
+                            <input onChange={handleChange} type="email" name="email" className="form-control" id="email" defaultValue={email}/>
                         </div>
                         <div className="form-floating">
                             <label className="form-text">message</label>
-                            <textarea className="form-control" placeholder="leave a message and I'll get back to you soon! :D" id="message" style={{ height: "100px" }}></textarea>
+                            <textarea onChange={handleChange} name="message" className="form-control" placeholder="leave a message and I'll get back to you soon! :D" defaultValue={message} id="message" style={{ height: "100px" }}></textarea>
                         </div>
                         <button type="submit" className="btn contact-btn mt-4">enter</button>
                     </form>
