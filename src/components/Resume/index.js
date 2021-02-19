d7a620cc9bda6349fb00d2c86e6b26ea990a424e
melissikiss <melissikiss@gmail.com>
2021-02-19 12:20:04 -0800

    added resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import myResume from '../../assets/profile-photo/resume.pdf';
+import myResume from '../../assets/profile-photo/Melissa_Barrera_Resume.PDF';
 
 function Resume() {
     return (
         <section className="container">
             <div className="text-center m-5">
                 <p className="resume-download">For a more detailed list of skills I have to offer, feel free to go over my resume</p>
-                <a href={myResume} download="MelissaResume" className="resume-download-icon"><i class="far fa-file"></i></a>
+                <a href={myResume} download="Melissa_Barrera_Resume" className="resume-download-icon"><i class="far fa-file"></i></a>
             </div>
         </section>
 
