dca546d5efa26455020dfb467c88e6837e4b3923
unknown <melissikiss@gmail.com>
2021-07-22 22:20:22 -0700

    editing styling on portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,34 @@
 import React from 'react'; 
+import Helmet from 'react-helmet';
+
 import Project from '../Project'; 
+import video from '../../assets/images/practice.webm';
+import gif from '../../assets/images/gif.gif'; 
+
 
 function Portfolio() {
     
     return (
-        <section>
-            <h1 className="text-center m-5 portfolio-text">Projects I've Worked On</h1>
-            <Project />
+        
+        <section className = "projects-div container">
+            <Helmet>
+                <style>
+                    {`
+                            body {
+                                background-color: #191A1D;
+                            }
+                       `}
+                </style>
+            </Helmet>
+            <div className = "col-md-6 projects-div">
+            <h1 className="text-left portfolio-text">Projects</h1>
+            <h4 className = "text-left portfolio-text">Front end. Back end. Full-Stack.</h4>
+            <h5 className = "text-left portfolio-text">Here are some of the projects I've worked on.</h5>
+            </div>
+            <div className = "col-md-6"> 
+            
+            </div>
+            {/* <Project /> */}
         </section>
     );
 }
