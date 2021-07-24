6004921aa9ec1e716f10084a74209c7e50749296
unknown <melissikiss@gmail.com>
2021-07-24 11:34:00 -0700

    added front end GET request for messages

--- client/src/components/SuperSecret.js/index.js
+++ client/src/components/SuperSecret.js/index.js
@@ -1,11 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function SuperSecret() {
+    const [data, setData] = useState('');
+    useEffect(() => {
+        fetch("/api/messages")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error({ message: 'Something went wrong!' });
+                }
+                return response.json();
+            })
+            .then(data => {
+                setData(data)
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [])
+
+    console.log(data)
+
+    // console.log(messages)
     return (
-        <div> 
-            this is a super secret page
+        <div className="container">
+            <div className="row align-items-center">
+                {data.map((message) => (
+                    <div className = "col-md-6">
+                        <p>{message.name}</p>
+                        <p>{message.email}</p>
+                        <p>{message.message}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
-}; 
+}
 
-export default SuperSecret; 
+
+
+
+export default SuperSecret;
